7e92c934af24f902ae4396422d0c9d37e324ef67
nadaAbedAlkreem <elkaloutnada@gmail.com>
2025-07-20 19:19:28 +0300

    feat(phone) : add request validation

--- resources/views/dashboard/auth/layout.blade.php
+++ resources/views/dashboard/auth/layout.blade.php
@@ -15,7 +15,7 @@ License: For each use you must have a valid license purchased only from above li
 <!--begin::Head-->
 <head>
     <base href="../../../">
-    <title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel
+    <title>أين علاجي؟ - تطبيق العثور على العيادات والخدمات الطبية</title>
         by Keenthemes</title>
     <meta charset="utf-8"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -47,8 +47,8 @@ License: For each use you must have a valid license purchased only from above li
         <!--begin::Content-->
         <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
             <!--begin::Logo-->
-            <a href="../../demo15/dist/index.html" class="mb-12">
-                <img alt="Logo" src="assets/media/logos/logo-1.svg" class="h-40px"/>
+            <a   class="mb-12">
+                <img alt="Logo" src="assets/media/logos/favicon.ico" class="h-40px"/>
             </a>
             <!--end::Logo-->
             <!--begin::Wrapper-->
--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -1,31 +1,18 @@
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
+
 <html lang="ar" dir="rtl">
 <!--begin::Head-->
 <head><base href="../../">
-    <title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel by Keenthemes</title>
+    <title>أين علاجي؟ - تطبيق العثور على العيادات والخدمات الطبية</title>
     <meta charset="utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="keywords" content="where my treatment" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="ar_SA" />
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Keenthemes | Metronic" />
-    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <meta property="og:title" content="where my treatment" />
+    <meta property="og:site_name" content="where my treatment" />
+    <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}" />
     <!--begin::Fonts-->
    @include('dashboard.assets.css')
 
